7fe220a6d838a6bfa92bddb22bdf2423069c5a39
Simran-99 <simran.sohal332@gmail.com>
2024-06-13 08:13:21 -0600

    Frontend Header created

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// For body we would have 2 divs, one will have body and the other will have the body the other would have the texts
 const Body = () => {
     return (
         <div>Body</div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
         <div className='flex justify-between'>
             <div className='flex items-center gap-2'>
             <img src="./assets/Logo.svg" alt="logo"></img>
-            <button>Hoster is hiring</button>
+            {/* On the button provided in the template, the button is a gradient */}
+            {/* The size of the text in the button is changed to small and padding is provided as needed */}
+            <button className='bg-gradient-to-r from-orange-400 to-red-500 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hoster is hiring</button>
             </div>
             <div>
                 <FaBars></FaBars>
