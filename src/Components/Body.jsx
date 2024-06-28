020368f3a5b704109231a58a81fe7f53e8adb1e8
Simran-99 <simran.sohal332@gmail.com>
2024-06-28 11:38:34 -0600

    Complete front end using HTML and CSS

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-// For body we would have 2 divs, one will have body and the other will have the body the other would have the texts
+import React from 'react';
+
 const Body = () => {
     return (
-        <div>Body</div>
-    )
+        <div className='space-y-4 lg:flex'>
+            <div className='relative flex items-center justify-center lg:flex-1'>
+                <img src='./assets/Blue-Shape.svg' alt="lst" className='-rotate-[45deg] h-64 md:h-72' />
+                <img src='./assets/Pink-Shape.svg' alt="lst" className='absolute -rotate-[30deg] h-64 md:h-72' />
+                <img src='./assets/Purple-Shape.svg' alt="lst" className='absolute -rotate-[15deg] h-64 md:h-72' />
+                <img src='./assets/Hero-Model.png' alt="lst" className='absolute h-64 md:h-72' />
+            </div>
+            <div className='lg:flex-1'>
+                <h1 className='text-5xl font-bold font-playfair leading-tight'>Host your website in less than 5 minutes</h1>
+                <p className='font-lato text-gray-400'>With Hoster, get your website up and running in no less than 5 minutes with the most competitive pricing packages available online.</p>
+                <form className='flex flex-col gap-4 md:flex-row'>
+                    <input className='rounded-md px-4 py-3 placeholder:text-gray-400' type='email' placeholder='Enter email address' />
+                    <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'>Join waitlist</button>
+                </form>
+                <div className='flex gap-2'>
+                    <img src='./assets/Checkmark.svg' alt='check' />
+                    <p className='font-lato text-gray-400'>No spam, Unsubscribe anytime</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Body
+export default Body;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,16 @@ const Header = () => {
             {/* The size of the text in the button is changed to small and padding is provided as needed */}
             <button className='bg-gradient-to-r from-orange-400 to-red-500 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hoster is hiring</button>
             </div>
-            <div>
+            <ul className='hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6'>
+                <li><a href='#'>Plans</a></li>
+                <li><a href='#'>Find Domains</a></li>
+                <li><a href='#'>Why hoster?</a></li>
+            </ul>
+            <div className='hidden lg:flex justify-center items-center font-lato gap-6'>
+                <a className='text-gray-400' href='#'> Sign In</a>
+                <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'> Join waitlist</button>
+            </div>
+            <div className='lg:hidden'>
                 <FaBars></FaBars>
                 </div></div>
     )
